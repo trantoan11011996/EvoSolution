a2e6389aaf2a09104e0035d9407013462db68124
toan <trantoan11011996@gmail.com>
2023-05-06 00:48:54 +0700

    translate

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import English from "../i18n/en.json";
 import Vietnamese from "../i18n/vi.json";
+import Backend from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 const getLocalLang = localStorage.getItem("lang");
 console.log("getLocal", getLocalLang);
@@ -14,13 +16,20 @@ const resources = {
   },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  debug: true,
-  lng: getLocalLang,
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(Backend)
+
+  .use(LanguageDetector)
+
+  .use(initReactI18next)
+  .use(initReactI18next)
+  .init({
+    resources,
+    debug: true,
+    lng: getLocalLang,
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default i18n;
